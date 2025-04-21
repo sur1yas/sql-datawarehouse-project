31e8a1b061cd8317d4ec387c0ce2e4806131e9ae
sur1yas <sanathsurya42@gmail.com>
2025-04-21 16:29:34 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -25,16 +25,18 @@ CREATE TABLE silver.crm_cust_info(
 
 -- Create crm_prd_info
 IF OBJECT_ID('silver.crm_prd_info','U') IS NOT NULL
-		DROP TABLE silver.crm_prd_info
+	DROP TABLE silver.crm_prd_info
 CREATE TABLE silver.crm_prd_info(
 		prd_id			INT,
+		cat_id			NVARCHAR(50),
 		prd_key			NVARCHAR(50),
 		prd_nm			NVARCHAR(50),
 		prd_cost		INT,
 		prd_line		NVARCHAR(50),
 		prd_start_dt	DATE,
-		prd_end_dt		DATE
-  );
+		prd_end_dt		DATE,
+		dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
 
 
 -- Create crm_sales_details
