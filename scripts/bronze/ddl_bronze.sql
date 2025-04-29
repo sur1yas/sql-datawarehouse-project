1ade6db4243494cdf50747ea8a6f8bb40a13ab32
sur1yas <sanathsurya42@gmail.com>
2025-04-29 14:03:46 +0530

    Create ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,5 +1,4 @@
 -- Naming convention: schema.source_filename
--- Cannot use CREATE OR REPLACE TABLE in SQL
 
 -- Create crm_cust_info
 IF OBJECT_ID('bronze.crm_cust_info','U') IS NOT NULL
@@ -9,11 +8,11 @@ GO
 CREATE TABLE bronze.crm_cust_info(
 		cst_id				INT,
 		cst_key				NVARCHAR(50),
-		cst_firstname		NVARCHAR(50),
-		cst_lastname		NVARCHAR(50),
-		cst_marital_status  NVARCHAR(50),
+		cst_firstname			NVARCHAR(50),
+		cst_lastname			NVARCHAR(50),
+		cst_marital_status 	 	NVARCHAR(50),
 		cst_gndr			NVARCHAR(50),
-		cst_create_date		DATETIME
+		cst_create_date			DATETIME
 );
 GO
 
@@ -28,7 +27,7 @@ CREATE TABLE bronze.crm_prd_info(
 		prd_nm			NVARCHAR(50),
 		prd_cost		INT,
 		prd_line		NVARCHAR(50),
-		prd_start_dt	DATETIME,
+		prd_start_dt		DATETIME,
 		prd_end_dt		DATETIME
 );
 GO
@@ -42,11 +41,11 @@ CREATE TABLE bronze.crm_sales_details(
 		sls_ord_num		NVARCHAR(50),
 		sls_prd_key		NVARCHAR(50),
 		sls_cust_id		INT,
-		sls_order_dt	DATETIME,
+		sls_order_dt		DATETIME,
 		sls_ship_dt		DATETIME,
 		sls_due_dt		DATETIME,
 		sls_sales		INT,
-		sls_quantity	INT,
+		sls_quantity		INT,
 		sls_price		INT
 );
 GO
@@ -57,9 +56,9 @@ IF OBJECT_ID('bronze.erp_cust_az12','U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.erp_cust_az12(
-		cid				NVARCHAR(50),
+		cid			NVARCHAR(50),
 		bdate			DATETIME,
-		gen				NVARCHAR(50)
+		gen			NVARCHAR(50)
 );
 GO
 
@@ -69,7 +68,7 @@ IF OBJECT_ID('bronze.erp_loc_a101','U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.erp_loc_a101(
-		cid				NVARCHAR(50),
+		cid			NVARCHAR(50),
 		cntry			NVARCHAR(50)
 );
 GO
@@ -80,8 +79,8 @@ IF OBJECT_ID('bronze.erp_px_cat_g1v2','U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.erp_px_cat_g1v2(
-		id				NVARCHAR(50),
-		cat				NVARCHAR(50),
+		id			NVARCHAR(50),
+		cat			NVARCHAR(50),
 		subcat			NVARCHAR(50),
 		maintenance		NVARCHAR(50)
 );
