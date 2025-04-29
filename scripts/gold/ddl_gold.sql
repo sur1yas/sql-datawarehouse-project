824383d2977adf7934febbfdb9ca47f32cbd6e35
sur1yas <sanathsurya42@gmail.com>
2025-04-29 23:00:15 +0530

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -72,8 +72,8 @@ GO
 CREATE VIEW gold.fact_sales AS
 SELECT
     sd.sls_ord_num  AS order_number,
-    pr.product_key  AS product_key,
-    cu.customer_key AS customer_key,
+    pr.product_key  AS product_key,                          -- Foreign Key(dim_products -> surrogate key)
+    cu.customer_key AS customer_key,                         -- Foreign Key(dim_customers -> surrogate key)
     sd.sls_order_dt AS order_date,
     sd.sls_ship_dt  AS shipping_date,
     sd.sls_due_dt   AS due_date,
@@ -81,8 +81,8 @@ SELECT
     sd.sls_quantity AS quantity,
     sd.sls_price    AS price
 FROM silver.crm_sales_details sd
-LEFT JOIN gold.dim_products pr
-    ON sd.sls_prd_key = pr.product_number
-LEFT JOIN gold.dim_customers cu
+LEFT JOIN gold.dim_products pr                               -- Data Lookup ie connecting the Dim w Fact
+    ON sd.sls_prd_key = pr.product_number                    
+LEFT JOIN gold.dim_customers cu                              -- Data Lookup ie connecting the Dim w Fact
     ON sd.sls_cust_id = cu.customer_id;
 GO
